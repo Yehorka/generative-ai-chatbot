c5a800247c258e9c4a06113f91a374cc1bd22014
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-02 16:42:29 +0300

    feat: add create user view

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView, Response
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserView(CreateAPIView):
+    model = User
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer

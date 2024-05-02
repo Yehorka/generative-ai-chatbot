8406acd88464ae2dae61198c5a1115cb6954503c
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-02 12:38:15 +0300

    feat: remove NewChatSerialiser; remove create method from MessageSserialiser

--- src/chat/serializers.py
+++ src/chat/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Chat, Message
 
 
-class ChatListSerilizer(serializers.ModelSerializer):
+class ChatSerilizer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = ['id']
@@ -11,26 +11,6 @@ class ChatListSerilizer(serializers.ModelSerializer):
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ['chat', 'role', 'content']
-        read_only_fields = ['role']
+        fields = ['role', 'content']
+        read_only = ['roel']
 
-    def create(self, validated_data):
-        chat = Chat.objects.get(pk=validated_data["chat"])
-        return Message(
-            chat=chat,
-            roel=Message.RoleChoices.USER,
-            content=validated_data["message"],
-        )
-
-
-class NewChatSerialiser(serializers.Serializer):
-    user = serializers.UUIDField(required=False)
-    message = serializers.CharField(required=True)
-
-    def create(self, validated_data):
-        chat = Chat(user=self.validated_data["user"])
-        return Message(
-            chat=chat,
-            roel=Message.RoleChoices.USER,
-            content=validated_data["message"],
-        )

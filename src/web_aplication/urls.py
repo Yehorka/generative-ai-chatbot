e4f691cfac15b0baac60a3e514a9e8f99fd8c76e
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-02 16:43:20 +0300

    feat: add user reqister url

--- src/web_aplication/urls.py
+++ src/web_aplication/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/chat/', include('chat.urls'))
+    path('api/chat/', include('chat.urls')),
+    path('api/users/', include('users.urls')),
 ]

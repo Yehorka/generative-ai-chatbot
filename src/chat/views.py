0f26bb244269e3a0e79a2b6d402a5cb993cba31f
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-02 17:15:24 +0300

    feat: add user filter to chats

--- src/chat/views.py
+++ src/chat/views.py
@@ -1,16 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
+from django.core.validators import ValidationError
+from django.contrib.auth.models import User
 
 from .models import Chat, Message
 from .serializers import ChatSerilizer, MessageSerializer  
 from .services import get_ai_response
 
 
+def get_chat(chat_id: str, user: User) -> Chat:
+    try:
+        chat = Chat.objects.filter(user=user, pk=chat_id).first()
+        if not chat:
+            raise Http404
+        return chat
+    except ValidationError:
+        raise Http404
+
+
 class ChatView(APIView):
     def get(self, request, chat_id=None):
         if chat_id:
-            chat = Chat.objects.get(pk=chat_id)
+            chat = get_chat(chat_id, request.user)
             messages = Message.objects.filter(chat=chat).order_by('timestamp')
             serializer = MessageSerializer(messages, many=True)
             return Response(serializer.data)
@@ -21,7 +34,7 @@ class ChatView(APIView):
         
     def post(self, request, chat_id=None,):
         if chat_id:
-            chat = Chat.objects.get(pk=chat_id)
+            chat = get_chat(chat_id, request.user)
         else:
             chat = Chat(user=request.user)
             chat.save()

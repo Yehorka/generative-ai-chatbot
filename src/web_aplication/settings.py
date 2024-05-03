400db6eedfef23da1c69af1a447c37e9056c39f5
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-03 20:50:32 +0300

    feat: add JWT auth

--- src/web_aplication/settings.py
+++ src/web_aplication/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'chat.apps.ChatConfig',
     'users.apps.UsersConfig'
@@ -130,8 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',

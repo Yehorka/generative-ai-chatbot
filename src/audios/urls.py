1ca389526575ea28e2f347e8ff4e4e944c6b1b44
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-22 17:23:19 +0300

    feat: add speech-to-text url

--- src/audios/urls.py
+++ src/audios/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('text-to-speach/', views.text_to_speech_view, name='text-to-speech'),
+    path('text-to-speech/', views.text_to_speech_view, name='text-to-speech'),
+    path('speech-to-text/', views.speech_to_text_view, name='speech-to-text'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

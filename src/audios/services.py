bf90b08185490688fb05e9e0dd8956b7b656ec53
Serhii Chukin <chukin2003.04@gmail.com>
2024-05-22 17:18:23 +0300

    feat: add speech_to_tex fuction

--- src/audios/services.py
+++ src/audios/services.py
@@ -10,3 +10,12 @@ def text_to_speech(file_path: Path, data: dict) -> None:
     client = get_client()
     response = client.audio.speech.create(**data)
     response.write_to_file(file_path)
+
+
+def speech_to_text(file_path: Path) -> str:
+    client = get_client()
+    audio_file = open(file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file
+    )
+    return transcription.text

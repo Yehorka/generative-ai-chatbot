c325ed7a48f1b15ac3fe010a451fd6caebd92c4b
Yehorka <25kabachok25@gmail.com>
2025-11-27 15:55:31 +0200

    env and key length change

--- src/apis/models.py
+++ src/apis/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class APIKey(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    key = models.CharField(max_length=100)
+    key = models.CharField(max_length=200)
 
 
 class InstructionFile(models.Model):
